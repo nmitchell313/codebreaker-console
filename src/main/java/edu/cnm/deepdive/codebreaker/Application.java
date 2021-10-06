ca4dae03d4678b1f682f7514f0c53bf780e64421
nmitchell313 <nmitchel11@cnm.edu>
2021-10-06 17:04:40 -0600

    defined model classes and proxy interface(in progress).

--- src/main/java/edu/cnm/deepdive/codebreaker/Application.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Application.java
@@ -1,15 +1,38 @@
 package edu.cnm.deepdive.codebreaker;
 
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.service.WebServiceProxy;
+import retrofit2.Call;
+import retrofit2.Response;
+import java.io.IOException;
+
 public class Application {
 
-    public static void main(String[] args) {
-    // TODO Read command line-arguments for pool and length
-    // TODO Make request to codebreaker service to start game with a new code.
-    // TODO While code is not guessed:
-    //  1. Read guess from user input.
-    //  2. Submit guess to codebreaker service.
-    //  3. Display guess results.
+    public static void main(String[] args) throws IOException {
+        // TODO Read command line-arguments for pool and length
 
+        String pool = "ABCDEF"; // fixme
+        int length = 3; // fixme read from args
+        Game game = startGame(pool, length);
+        System.out.printf("Game id = %s%n", game.getId());
+
+        // TODO While code is not guessed:
+        //  1. Read guess from user input.
+        //  2. Submit guess to codebreaker service.
+        //  3. Display guess results.
+    }
+
+    private static Game startGame(String pool, int length) throws IOException {
+        WebServiceProxy proxy = WebServiceProxy.getInstance();
+        Game game = new Game();
+        game.setPool(pool);
+        game.setLength(length);
+        Call<Game> call = proxy.startGame(game);
+        Response<Game> response = call.execute();
+        if (!response.isSuccessful()) {
+            throw new RuntimeException(response.message());
+        }
+        return response.body();
     }
 
 }
--- src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
@@ -2,10 +2,27 @@ package edu.cnm.deepdive.codebreaker.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.model.Guess;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface WebServiceProxy {
+
+    @POST("codes")
+    Call<Game> startGame(@Body Game game);
+
+    @POST("codes/{gameId}/guesses")
+    Call<Guess> submitGuess(@Body Guess guess,@Path("gameId") String gameId);
+
+
+    static WebServiceProxy getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
 
     class InstanceHolder {
 
@@ -17,7 +34,7 @@ public interface WebServiceProxy {
                     .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
                     .create();
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("https://ddc-java.service/codebreaker/")
+                    .baseUrl("https://ddc-java.services/codebreaker/")
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
             INSTANCE = retrofit.create(WebServiceProxy.class);
