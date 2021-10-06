668780209b60211a0c84c3ba327aad5291504a7c
nmitchell313 <nmitchel11@cnm.edu>
2021-10-06 15:17:30 -0600

    defined model classes and proxy interface(in progress).

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public interface WebServiceProxy {
+
+    class InstanceHolder {
+
+        private static final WebServiceProxy INSTANCE;
+
+        static {
+            Gson gson = new GsonBuilder()
+                    .excludeFieldsWithoutExposeAnnotation()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+                    .create();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://ddc-java.service/codebreaker/")
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            INSTANCE = retrofit.create(WebServiceProxy.class);
+        }
+    }
+}
+
